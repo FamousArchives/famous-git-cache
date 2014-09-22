e6c721cfe19262d0b9455f4320e956ffcb66d4c8
Myles Borins <myles@famo.us>
2014-09-22 11:27:11 -0700

    tests passing

--- bin/cmd.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-/*eslint no-process-exit:0*/
-'use strict';
-var argv = require('minimist')(process.argv.slice(2));
-var lib = require('../lib');
-
-console.log('argv:', argv);
-
-process.exit(0);
